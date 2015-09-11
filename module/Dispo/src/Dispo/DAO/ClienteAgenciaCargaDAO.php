a21ca70fae390a77f11ac41051b7a140cb1f3550
LinuxTorvalds <wilmer@agrinag.com>
2015-09-11 16:48:19 -0500

    editador cliente agenciacarga dao

--- module/Dispo/src/Dispo/DAO/ClienteAgenciaCargaDAO.php
+++ module/Dispo/src/Dispo/DAO/ClienteAgenciaCargaDAO.php
@@ -10,29 +10,21 @@ use Doctrine\ORM\Tools\Pagination\Paginator;
 
 class ClienteAgenciaCargaDAO extends Conexion 
 {
-	private $table_name	= 'agencia_carga';
+	private $table_name	= 'cliente_agencia_carga';
 
 	/**
 	 * Ingresar
 	 *
-	 * @param AgenciaCargaData $AgenciaCargaData
+	 * @param ClienteAgenciaCargaData $ClienteAgenciaCargaData
 	 * @return array Retorna un Array $key el cual contiene el id
 	 */
-	public function ingresar(AgenciaCargaData $AgenciaCargaData)
+	public function ingresar(ClienteAgenciaCargaData $ClienteAgenciaCargaData)
 	{
 		$key    = array(
-				'id'						        => $AgenciaCargaData->getId(),
+				'cliente_id'						        => $ClienteAgenciaCargaData->getClienteId(),
 		);
 		$record = array(
-				'id'								=> $AgenciaCargaData->getId(),
-				'nombre'		                    => $AgenciaCargaData->getNombre(),
-				'direccion'		            		=> $AgenciaCargaData->getDireccion(),
-				'telefono'                			=> $AgenciaCargaData->getTelefono(),
-				'tipo'                				=> $AgenciaCargaData->getTipo(),
-				'estado'                			=> $AgenciaCargaData->getEstado(),
-				'fec_ingreso'                		=> \Application\Classes\Fecha::getFechaHoraActualServidor(),
-				'usuario_ing_id'                	=> $AgenciaCargaData->getUsuarioIngId(),
-				'sincronizado'                		=> 0
+				'agencia_carga_id'		            => $ClienteAgenciaCargaData->getAgenciaCargaId()
 		);
 		$this->getEntityManager()->getConnection()->insert($this->table_name, $record);
 		//$id = $this->getEntityManager()->getConnection()->lastInsertId();
@@ -47,144 +39,19 @@ class ClienteAgenciaCargaDAO extends Conexion
 	 * @param AgenciaCargaData $AgenciaCargaData
 	 * @return array Retorna un Array $key el cual contiene el id
 	 */
-	public function modificar(AgenciaCargaData $AgenciaCargaData)
+	public function eliminar(ClienteAgenciaCargaData $ClienteAgenciaCargaData)
 	{
 		$key    = array(
-				'id'						        => $AgenciaCargaData->getId(),
+				'cliente_id'						=> $ClienteAgenciaCargaData->getId(),
 		);
 		$record = array(
-				'nombre'		                    => $AgenciaCargaData->getNombre(),
-				'direccion'		            		=> $AgenciaCargaData->getDireccion(),
-				'telefono'                			=> $AgenciaCargaData->getTelefono(),
-				'tipo'                				=> $AgenciaCargaData->getTipo(),
-				'estado'                			=> $AgenciaCargaData->getEstado(),
-				'fec_modifica'                		=> \Application\Classes\Fecha::getFechaHoraActualServidor(),
-				'usuario_mod_id'                	=> $AgenciaCargaData->getUsuarioModId(),
-				'sincronizado'       	         	=> 0
-				//'fecha_mod'							=> \Application\Classes\Fecha::getFechaHoraActualServidor(),
+				'agencia_carga_id'            		=> $ClienteAgenciaCargaData->getAgenciaCargaId()
 		);
 		$this->getEntityManager()->getConnection()->update($this->table_name, $record, $key);
-		return $AgenciaCargaData->getId();
+		return $ClienteAgenciaCargaData->getClienteId();
 	}//end function modificar
 
 
-	/**
-	 * Consultar
-	 * 
-	 * @param string $id
-	 * @param int $resultType
-	 * @return \Dispo\Data\AgenciaCargaData|NULL|array
-	 */
-	public function consultar($id, $resultType = \Application\Constants\ResultType::OBJETO)
-	{
-		switch ($resultType)
-		{
-			case \Application\Constants\ResultType::OBJETO:
-				$AgenciaCargaData 		    = new AgenciaCargaData();
-				
-				$sql = 	' SELECT agencia_carga.* '.
-						' FROM agencia_carga '.
-						' WHERE agencia_carga.id = :id ';
-				
-				$stmt = $this->getEntityManager()->getConnection()->prepare($sql);
-				$stmt->bindValue(':id',$id);
-				$stmt->execute();
-				$row = $stmt->fetch();  //Se utiliza el fecth por que es un registro
-				if($row){
-					$AgenciaCargaData->setId			    ($row['id']);
-					$AgenciaCargaData->setNombre		    ($row['nombre']);
-					$AgenciaCargaData->setDireccion		    ($row['direccion']);
-					$AgenciaCargaData->setTelefono    		($row['telefono']);
-					$AgenciaCargaData->setTipo				($row['tipo']);
-					$AgenciaCargaData->setEstado    		($row['estado']);
-					$AgenciaCargaData->setFecIngreso   		($row['fec_ingreso']);
-					$AgenciaCargaData->setFecModifica  		($row['fec_modifica']);
-					$AgenciaCargaData->setUsuarioIngId		($row['usuario_ing_id']);
-					$AgenciaCargaData->setUsuarioModId		($row['usuario_mod_id']);
-					$AgenciaCargaData->setSinronizado		($row['sincronizado']);
-					$AgenciaCargaData->setFecSincronizado	($row['fec_sincronizado']);
-				
-					return $AgenciaCargaData;
-				}else{
-					return null;
-				}//end if				
-				break;
-				
-			case \Application\Constants\ResultType::MATRIZ:
-				$sql = 	' SELECT agencia_carga.*, usuario_ing.username as usuario_ing_user_name, usuario_mod.username as usuario_mod_user_name  '.
-						' FROM agencia_carga LEFT JOIN usuario as usuario_ing '.
-						'                           ON usuario_ing.id = agencia_carga.usuario_ing_id '.
-						'					 LEFT JOIN usuario as usuario_mod '.
-						'                           ON usuario_mod.id = agencia_carga.usuario_mod_id '.						
-						' WHERE agencia_carga.id = :id ';
-				
-				$stmt = $this->getEntityManager()->getConnection()->prepare($sql);
-				$stmt->bindValue(':id',$id);
-				$stmt->execute();
-				$row = $stmt->fetch();  //Se utiliza el fecth por que es un registro
-				return $row;
-				break;
-		}//end switch
-
-
-	}//end function consultar
-
-	
-	
-	/**
-	 *
-	 * @param string $id
-	 * @param string $nombre
-	 * @return array
-	 */
-	public function consultarDuplicado($accion, $id, $nombre)
-	{
-		$sql = 	' SELECT agencia_carga.*, usuario_ing.username as usuario_ing_user_name, usuario_mod.username as usuario_mod_user_name  '.
-				' FROM agencia_carga LEFT JOIN usuario as usuario_ing '.
-				'                           ON usuario_ing.id = agencia_carga.usuario_ing_id '.
-				'					 LEFT JOIN usuario as usuario_mod '.
-				'                           ON usuario_mod.id = agencia_carga.usuario_mod_id ';
-		switch ($accion)
-		{
-			case 'I':
-					$sql = $sql." WHERE agencia_carga.id 	 = '".$id."'".
-					            "    or agencia_carga.nombre = '".$nombre."'";
-				break;
-				
-			case 'M':
-				$sql = $sql." WHERE agencia_carga.id 	!= '".$id."'".
-						    "   and agencia_carga.nombre = '".$nombre."'";
-				break;
-		}//end switch
-	
-		$stmt = $this->getEntityManager()->getConnection()->prepare($sql);
-		$stmt->execute();
-		$result = $stmt->fetchAll();  //Se utiliza el fecth por que es un registro
-		return $result;
-	}//end function consultarDuplicado
-
-
-	/**
-	 * 
-	 * @param string $estado
-	 * @return array
-	 */
-	public function consultarTodos($estado = NULL)
-	{
-		$sql = 	' SELECT agencia_carga.* '.
-				' FROM agencia_carga '.
-				' WHERE 1=1';
-	
-		if (!empty($estado)){
-			$sql = $sql." and estado = '".$estado."'";
-		}//end if
-
-		$stmt = $this->getEntityManager()->getConnection()->prepare($sql);
-		$stmt->execute();
-		$result = $stmt->fetchAll();  //Se utiliza el fecth por que es un registro
-	
-		return $result;
-	}//end function consultarTodos
 
 	
 	
@@ -213,6 +80,7 @@ class ClienteAgenciaCargaDAO extends Conexion
 	
 		return $result;
 	}//end function listado
+	
 	
 	
 	/**
