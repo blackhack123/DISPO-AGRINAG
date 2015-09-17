f357ffc7bd0607f84e4e8d47316d4c85cc0254cf
LinuxTorvalds <wilmer@agrinag.com>
2015-09-17 16:13:06 -0500

    implemetando getcombo grupodispo

--- module/Dispo/src/Dispo/Controller/GrupodispoController.php
+++ module/Dispo/src/Dispo/Controller/GrupodispoController.php
@@ -515,7 +515,43 @@ class GrupodispoController extends AbstractActionController
 	
 	
 	
-	
+	function getcomboAction()
+	{
+		try
+		{
+			$EntityManagerPlugin = $this->EntityManagerPlugin();
+				
+			$GrupoDispoCabBO = new GrupoDispoCabBO();
+			$GrupoDispoCabBO->setEntityManager($EntityManagerPlugin->getEntityManager());
+		
+			$SesionUsuarioPlugin = $this->SesionUsuarioPlugin();
+			$SesionUsuarioPlugin->isLoginClienteVendedor();
+		
+			$body = $this->getRequest()->getContent();
+			$json = json_decode($body, true);
+			//var_dump($json); exit;
+			$texto_primer_elemento		= $json['texto_primer_elemento'];
+			//$cliente_id = $SesionUsuarioPlugin->getUserClienteId();
+		
+			//$opciones 	= $MarcacionBO->getComboPorClienteId($cliente_id, 0, $texto_primer_elemento);
+			$opciones 	= $GrupoDispoCabBO->getComboGrupoDispo($grupo_dispo_cab_id, 0, $texto_primer_elemento);
+			
+			$response = new \stdClass();
+			$response->opciones				= $opciones;
+			$response->respuesta_code 		= 'OK';
+		
+			$json = new JsonModel(get_object_vars($response));
+			return $json;
+		
+		}catch (\Exception $e) {
+			$excepcion_msg =  utf8_encode($this->ExcepcionPlugin()->getMessageFormat($e));
+			$response = $this->getResponse();
+			$response->setStatusCode(500);
+			$response->setContent($excepcion_msg);
+			return $response;
+		}
+		
+	}//end function getcomboAction
 	
 	
 }
