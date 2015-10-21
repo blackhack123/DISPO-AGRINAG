9e2f4e8c5840db3a1066f4ab461cb58a6eaad0fb
LinuxTorvalds <wilmer@agrinag.com>
2015-10-21 11:04:25 -0500

    REPORTE DISPO X GRUPO

--- module/Dispo/src/Dispo/Controller/GrupodispoController.php
+++ module/Dispo/src/Dispo/Controller/GrupodispoController.php
@@ -648,4 +648,44 @@ class GrupodispoController extends AbstractActionController
 		}		
 	}//end function grabarporgrupoporgradoAction
 	
-}
+	
+	
+	function exportarExcelDispoGrupoAction()
+	{
+		try
+		{
+			$viewModel 			= new ViewModel();
+			$EntityManagerPlugin = $this->EntityManagerPlugin();
+	
+			$GrupoDispoCabBO = new GrupoDispoCabBO();
+			$GrupoDispoCabBO->setEntityManager($EntityManagerPlugin->getEntityManager());
+	
+			$SesionUsuarioPlugin = $this->SesionUsuarioPlugin();
+			$SesionUsuarioPlugin->isLoginAdmin();
+	
+			$request 				= $this->getRequest();
+			
+			$grupo_dispo_cab_id 	= $request->getQuery('grupo_dispo_cab_id', "");
+			$color_ventas_id  		= $request->getQuery('color_ventas_id', "");
+			$calidad_variedad_id	= $request->getQuery('calidad_variedad_id', "");
+	
+			$condiciones = array(
+					"grupo_dispo_cab_id"		=> $grupo_dispo_cab_id,
+					"color_ventas_id"			=> $color_ventas_id,
+					"calidad_variedad_id"		=> $calidad_variedad_id
+			);
+			$result = $GrupoDispoCabBO->generarExcel($condiciones);
+	
+			exit;
+		}catch (\Exception $e) {
+			$excepcion_msg =  utf8_encode($this->ExcepcionPlugin()->getMessageFormat($e));
+			$response = $this->getResponse();
+			$response->setStatusCode(500);
+			$response->setContent($excepcion_msg);
+			return $response;
+		}
+	}//end function exportarexcel2Action
+	
+	
+	
+}//end class
